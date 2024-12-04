bbdcf023cb68c4679b0cd5cf1812bb6d9a036f18
Damji Ahir <damjidangar07@gmail.com>
2024-12-04 10:57:10 +0530

    set up html,css structure

--- src/App.js
+++ src/App.js
@@ -7,31 +7,46 @@ function App() {
       <h1>{rupeeSymbol} 500</h1>
       <div className="form">
         <form>
-          <input type="text" />
-          <input type="date" />
-          <input type="text" />
+          <div className="basic">
+            <input type="text" placeholder="Item" />
+            <input type="datetime-local" className="date" />
+          </div>
+          <div className="description">
+            <input type="text" placeholder="Description" />
+          </div>
           <button type="submit">Add transation</button>
         </form>
       </div>
       <div className="transactions">
         <div className="transaction">
           <div className="leftside">
-            <h4 className="name">a new samsung tv</h4>
+            <h4 className="name">Domain</h4>
             <p className="description">test</p>
           </div>
           <div className="rightside">
-            <h3 className="price">{rupeeSymbol} 500</h3>
+            <h4 className="amount-red">-{rupeeSymbol}2200</h4>
             <p className="date">03-12-2024</p>
+          </div>
+        </div>
+
+        <div className="transaction">
+          <div className="leftside">
+            <h4 className="name">Dig website</h4>
+            <p className="">test</p>
+          </div>
+          <div className="rightside">
+            <h4 className="amount-green">+{rupeeSymbol}10000</h4>
+            <p className="  date">03-12-2024</p>
           </div>
         </div>
         <div className="transaction">
           <div className="leftside">
-            <h4 className="name">a new samsung tv</h4>
-            <p className="description">test</p>
+            <h4 className="name"> Pythom cource</h4>
+            <p className="">test</p>
           </div>
           <div className="rightside">
-            <h3 className="price">{rupeeSymbol} 500</h3>
-            <p className="date">03-12-2024</p>
+            <h4 className="amount-red">- {rupeeSymbol}15000</h4>
+            <p className=" date">03-12-2024</p>
           </div>
         </div>
       </div>
